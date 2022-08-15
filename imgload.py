3a3a12940cb02dd6463bc488753d2481ce13834f
EISTIA <tpartner02@gmail.com>
2022-08-15 15:30:43 +0200

    loginform with tkinter GUI

---
+++ imgload.py
@@ -0,0 +1,19 @@
+
+
+
+import tkinter as tk
+from turtle import bgcolor
+
+root = tk.Tk()
+
+root.title('Image_Loader For VAE ')
+root.geometry('600x400+50+50')
+root.resizable(False, False)
+#root.iconbitmap('/home/eistia/Arl.png')
+root.configure(bg= '#0a630e')   #a essayer
+
+
+root.mainloop()
+
+
+
---
+++ loginform.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from functools import partial
+from turtle import bgcolor
+
+def validateLogin(username, password):
+	print("username entered :", username.get())
+	print("password entered :", password.get())
+	return
+
+#window
+tkWindow = Tk()  
+tkWindow.geometry('500x200+100+50')  
+tkWindow.title('GUI loginform')
+tkWindow.configure (bg="green")
+
+
+#username label and text entry box
+usernameLabel = Label(tkWindow,  width="16",bg="green",fg="white", text="Identifiant").grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow, width="40", textvariable=username).grid(row=0, column=1)  
+
+#password label and password entry box
+passwordLabel = Label(tkWindow, width="16",bg="green",fg="white",   text="Mot_de_passe").grid(row=1, column=0)  
+password = StringVar()
+passwordEntry = Entry(tkWindow,width="40", textvariable=password, show='*').grid(row=1, column=1)  
+
+validateLogin = partial(validateLogin, username, password)
+
+#login button
+loginButton = Button(tkWindow,width="13",fg="white", bg="#07870d", padx=20, text="Soumettre", command=validateLogin).grid(row=4, column=0)  
+
+tkWindow.mainloop()
